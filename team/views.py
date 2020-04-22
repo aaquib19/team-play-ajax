ca1dcf1a58574bf72ec272e7dec615e7ac6ea89f
Aaquib Niaz <aaquibniaz3600@gmail.com>
2020-04-22 16:02:29 +0530

    created base template

--- team/views.py
+++ team/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 # Create your views here.
-
-
+from .models import Team
+from player.models import Player
 def create_team(request):
-
+    print("in create team")
     if request.POST:
+        players  = request.POST.get("pids")
+        plist = players.split(",")
+        plist =plist[:-1]
+        tname  = request.POST.get("tname")
+        obj = Team.objects.create(
+            tname=tname
+        )
+        for id in plist:
+            player_obj = Player.objects.get(id=int(id))
+            obj.player.add(player_obj)
+        obj.save()
+        print(tname,players.split(","))
         print(request.POST)
-    else :
-        return render(request,'add-team.html',{})
+
+    return render(request,'add-team.html',{})

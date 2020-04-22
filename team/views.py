1ba7c6c98c2407031315da7348dc7304d4ecf36b
Aaquib Niaz <aaquibniaz3600@gmail.com>
2020-04-23 02:00:39 +0530

    created team add

--- team/views.py
+++ team/views.py
@@ -5,19 +5,29 @@ from player.models import Player
 def create_team(request):
     print("in create team")
     if request.POST:
-        players  = request.POST.get("pids")
-        plist = players.split(",")
-        plist =plist[:-1]
-        tname  = request.POST.get("tname")
-        obj = Team.objects.create(
-            tname=tname
-        )
-        for id in plist:
-            player_obj = Player.objects.get(id=int(id))
-            obj.player.add(player_obj)
-        obj.save()
-        print(tname,players.split(","))
+        player_ids =  request.POST.get('pids').split(",")
+        for i in player_ids:
+            print(i)
+            pname = request.POST.get('pname['+str(i) +']')
+            print(pname)
+        # players  = request.POST.get("pids")
+        # plist = players.split(",")
+        # plist =plist[:-1]
+        # tname  = request.POST.get("tname")
+        # obj = Team.objects.create(
+        #     tname=tname
+        # )
+        # for id in plist:
+        #     player_obj = Player.objects.get(id=int(id))
+        #     obj.player.add(player_obj)
+        # obj.save()
+        # print(tname,players.split(","))
         print(request.POST)
+
+        print(request.POST['pids'])
+        # print(len(lll))
+        # for i in lll:
+        #     print(i)
 
     return render(request,'add-team.html',{})
 
